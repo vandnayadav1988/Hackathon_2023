434365df9e35e4624bbee96749d2c36383fe72e5
unknown <vyadav24@yellowdc.com>
2023-09-17 15:15:59 +0530

    update

--- src/test/java/it/RestApiSchemaIntegrationTest.java
+++ src/test/java/it/RestApiSchemaIntegrationTest.java
@@ -9,7 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
-
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = Application.class)
 public class RestApiSchemaIntegrationTest {
@@ -22,7 +21,7 @@ public class RestApiSchemaIntegrationTest {
     String apiContract = RestApiSchemaIntegrationTest.class.getResource("/swagger-contract.yaml").getPath();
     System.out.println(apiContract);
     String swaggerSchemaUrl = String.format("http://localhost:%d/v2/api-docs", port);
-    SwaggerAssertions.assertThat(swaggerSchemaUrl)
-        .isEqualTo(apiContract);
+    SwaggerAssertions.assertThat(swaggerSchemaUrl).isEqualTo(apiContract);
   }
+
 }
